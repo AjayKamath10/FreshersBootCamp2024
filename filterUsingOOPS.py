9884c5883581b1a685039a6e03df9eff69c98fa7
AjayKamath10 <84276017+AjayKamath10@users.noreply.github.com>
2024-01-10 11:02:25 +0530

    Update filterUsingOOPS.py

--- filterUsingOOPS.py
+++ filterUsingOOPS.py
@@ -1,4 +1,49 @@
 class printer:
+    def __init__(self, item):
+        self.item = item
+        
+    def displayToScreen(self):
+        for obj in self.item:
+            print(obj.content())
+        
+class filterList:
+    
+    def __init__(self, itemList):
+        self.resultList = []
+        self.itemList = itemList
+        
+    def predicate(self, param):
+        for item in self.itemList:
+            if item.check(param):
+                self.resultList.append(item)
+        return self.resultList 
+                
+class stringFilter:
+    def __init__(self,string):
+        self.string = string
+        
+    def check(self,startChar):
+        return self.string[0].lower() == startChar.lower()
+        
+    def content(self):
+        return self.string
+                
+StringList = ["Carl", "Zeiss", "Ajay", "Air", "Car"]
+StringList = [stringFilter(string) for string in StringList]
+
+filterStringObj =  filterList(StringList)
+PrintObj = printer(filterStringObj.predicate('a'))
+
+PrintObj.displayToScreen()
+        
+    
+
+    
+    
+
+'''
+ALTERNATIVE:
+class printer:
     def __init__(self, string):
         self.string = string
         
@@ -29,7 +74,7 @@ filterStringObj =  filterList(StringList)
 
 PrintObj = printer(filterStringObj.predicate(stringFilter, stringFilter.checkStartsWith('a')))
 PrintObj.displayToScreen()
-        
+'''
     
 
     
