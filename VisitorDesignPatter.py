1c24d5f086134ffa33274c208620fdf82e5eb7d8
AjayKamath10 <84276017+AjayKamath10@users.noreply.github.com>
2024-01-12 14:43:48 +0530

    Create VisitorDesignPatter.py

---
+++ VisitorDesignPatter.py
@@ -0,0 +1,155 @@
+from abc import ABC, abstractmethod
+
+
+
+class DocumentPart(ABC):
+    _name = ''
+    _position = ''
+    
+    @abstractmethod
+    def paint(self):
+        pass
+    
+    @abstractmethod
+    def convert(self, converterClass):
+        pass
+ 
+    @abstractmethod
+    def save(self):
+        pass
+    
+    
+ 
+class Header(DocumentPart):
+    def __init__(self, title =  "DefaultTitle"):
+        self.title = title
+    def paint(self):
+        print("Painting Header titled", self.title)
+ 
+    def save(self):
+        print("Saving Header")
+        
+    def convert(self, converterClass):
+        converterClass.convertHeader(self)
+        pass
+ 
+class Footer(DocumentPart):
+    def __init__(self, text =  "DefaultText"):
+        self.text = text
+        
+    def paint(self):
+        print("Painting Footer having text", self.text)
+ 
+    def save(self):
+        print("Saving Footer")
+        
+    def convert(self, converterClass):
+        converterClass.convertFooter(self)
+        pass
+ 
+class Hyperlink(DocumentPart):
+    def __init__(self,url = "defaultURL.com", text = "defaultText"):
+        self.url = url
+        self.text = text
+    
+    def paint(self, ):
+        print("Painting Hyperlink having url {} and text {}".format(self.url, self.text))
+ 
+    def save(self):
+        print("Saving Hyperlink")
+        
+    def convert(self, converterClass):
+        converterClass.convertHyperLink(self)
+        pass
+ 
+class Paragraph(DocumentPart):
+    def __init__(self, content = "defaultContent"):
+        self.content = content
+        
+    def paint(self):
+        print("Painting Paragraph having content", self.content)
+ 
+    def save(self):
+        print("Saving Paragraph")
+        
+    def convert(self, converterClass):
+        converterClass.convertPara(self)
+        pass 
+ 
+
+class ConverterInterface(ABC):
+    
+    @abstractmethod
+    def convertHeader(self, header : Header):
+        pass
+    
+    @abstractmethod
+    def convertFooter(self, footer : Footer):
+        pass
+    
+    @abstractmethod
+    def convertHyperLink(self, hyperlink : Hyperlink):
+        pass
+    
+    @abstractmethod
+    def convertPara(self, paragraph : Paragraph):
+        pass
+    
+class HTMLConverter(ConverterInterface):
+    def convertHeader(self, header : Header):
+        print("<h1>"+header.title+"</h1>")
+        
+    
+    def convertFooter(self, footer : Footer):
+        print("<h2>"+footer.text+"</h2>")
+    
+    def convertHyperLink(self, hyperlink : Hyperlink):
+        print("<a href = {}>{}</a>".format(hyperlink.url, hyperlink.text))
+    
+    def convertPara(self, paragraph : Paragraph):
+        print("<p>{}</p>".format(paragraph.content))
+    
+    
+    
+class WordDocument:
+    def __init__(self, documentParts=[]):
+        self.documentParts = documentParts
+        
+    def addDocumentPart(self, documentPart):
+        self.documentParts.append(documentPart)
+ 
+    def openDocument(self):
+        for part in self.documentParts:
+            part.paint()
+ 
+    def saveDocument(self):
+        for part in self.documentParts:
+            part.save()
+            
+    
+ 
+def main():
+    headerObj = Header("Title1")
+    hyperlinkObj = Hyperlink("google.co.in", "search")
+    footerObj = Footer()
+    paragraphObj = Paragraph()
+    
+    documentPartList = [headerObj, footerObj, hyperlinkObj, paragraphObj] 
+    
+    wordDoc = WordDocument(documentPartList)
+    
+    HTMLConverterObj = HTMLConverter()
+    
+     
+    wordDoc.openDocument()
+    print()
+    wordDoc.saveDocument()
+    print()
+    print("Converting to HTML")
+    print("*"*25)
+
+    
+    for docPart in wordDoc.documentParts:
+        docPart.convert(HTMLConverterObj)
+    
+main()
