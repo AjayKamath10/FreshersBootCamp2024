8933c037ce0f95e2c3e86f07c43b1969f43ae43f
AjayKamath10 <84276017+AjayKamath10@users.noreply.github.com>
2024-01-15 11:23:50 +0530

    Update observerPattern.py

--- observerPattern.py
+++ observerPattern.py
@@ -1,25 +1,37 @@
 import time
+from abc import ABC, abstractmethod
+
 class Thread():
     def __init__(self):
         self.state = "created"
         self.priority = 10
         self.culture = "defaultCulture"
+        self.observerList = []
     
+    def __notify(self):
+        for observer in self.observerList:
+            observer.notify()
+        
     def start(self):
         self.state = "running"
+        self.__notify()
         
     def abort(self):
         self.state = "aborted"
+        self.__notify()
         
     def sleep(self, sleepInterval):
         self.state = "sleeping"
         time.sleep(sleepInterval)
+        self.__notify()
         
     def wait(self):
         self.state = 'waiting'
+        self.__notify()
         
-    def suspended(self):
+    def suspend(self):
         self.state = 'suspended'
+        self.__notify()
         
     def setPriority(self, newPriority):
         self.priority = newPriority
@@ -30,18 +42,53 @@ class Thread():
     def getState(self):
         return self.state
         
+    def subscribe(self, observerObj):
+        log = Log()
+        self.observerList.append(observerObj)
+        log.write("Added a subscriber")
+        
+    def unsubscribe(self, observerObj):
+        self.observerList.remove(observerObj)
+        
+        
+class IObserver(ABC):
+    
+    @abstractmethod
+    def update(self, stateString):
+        pass
+        
+class DashboardObserver(IObserver):
+    
+    def update(self,threadObj ,stateString):
+        
+        if stateString == 'wait':
+            threadObj.wait()
+            
+        elif stateString == 'suspend':
+            threadObj.suspend()
+            
+        #elif (other states)
+            
+    def notify(self):
+        log = Log()
+        log.write("Dashboard notified about state change")
+    
+
+class Log():
+    def write(self, string):
+        print(string)
 def main():
+    log = Log()
     Thread1 = Thread()
-    print("Thread state: ", Thread1.getState())
+    log.write(Thread1.getState())
     
-    Thread1.wait()
-    print("Thread state: ", Thread1.getState())
     
-    Thread1.abort()
-    print("Thread state: ", Thread1.getState())
+    Dashboard = DashboardObserver()
+    Thread1.subscribe(Dashboard)
     
-    Thread1.start()
-    print("Thread state: ", Thread1.getState())
+    Dashboard.update(Thread1, 'wait')
+    log.write(Thread1.getState())
+    
     
 main()
     
